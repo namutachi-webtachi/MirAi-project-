c0eb36ff88dc0822c89c811214936a4ce20e7200
namutachi-webtachi <namutachi20620@gmail.com>
2025-12-05 17:40:25 +0700

    config.js

--- config.js
+++ config.js
@@ -5,8 +5,13 @@ const CONFIG = {
     subTitle: "Namutachi",
     adminUser: "namutachi-webtachi", // Tên GitHub của cậu
     repoName: "MirAi-project-",      // Tên Repo
+     // 2. Social Media (Thêm link của bro vào đây)
+    social: {
+        facebook: "https://www.facebook.com/namutachi", // Link Fanpage
+        discord: "https://discord.gg/link-discord-cua-bro" // Link Discord
+    },
     
-    // 2. Cấu hình Bình luận (Giscus)
+    // 3. Cấu hình Bình luận (Giscus)
     // Vào https://giscus.app để lấy các mã này nếu chưa có
     giscus: {
         repo: "namutachi-webtachi/MirAi-comments",
@@ -15,6 +20,6 @@ const CONFIG = {
         categoryId: "DIC_kwDOQiwFrM4CzaA2" // <--- THAY MÃ CỦA CẬU VÀO ĐÂY
     },
 
-    // 3. Ảnh nền (Có thể thay link ảnh anime khác)
+    // 4. Ảnh nền (Có thể thay link ảnh anime khác)
     bgImage: "https://i.pinimg.com/1200x/b0/8f/59/b08f5925aeb1141b7cf18ea63f3185ae.jpg"
 };
